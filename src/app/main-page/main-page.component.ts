8fdd508269b129eab0e05e7547dc771e83fb915d
georgy.saukov <georgy.saukov@wirecard.com>
2020-10-03 20:41:19 +0200

    First loader, background screen

--- src/app/main-page/main-page.component.ts
+++ src/app/main-page/main-page.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import {AfterViewInit, Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html',
   styleUrls: ['./main-page.component.css']
 })
-export class MainPageComponent implements OnInit {
+export class MainPageComponent implements OnInit, AfterViewInit {
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  ngAfterViewInit(): void {
+    const loader = document.getElementById('firstLoader')
+    loader.style.display = "none";
   }
 
 }
